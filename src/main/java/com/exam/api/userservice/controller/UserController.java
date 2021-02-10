cd7183cc11c36c25ec3224f27c8da8810ee1c87e
sergiu221@gmail.com <sergiu221@gmail.com>
2021-02-10 17:38:18 +0100

    consume only JSON

--- src/main/java/com/exam/api/userservice/controller/UserController.java
+++ src/main/java/com/exam/api/userservice/controller/UserController.java
@@ -6,13 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.exam.api.userservice.dto.UserDto;
 import com.exam.api.userservice.model.CreateUserResponse;
@@ -20,7 +15,6 @@ import com.exam.api.userservice.model.CreateUserRequest;
 import com.exam.api.userservice.service.UserService;
 
 @RestController
-@RequestMapping("/users")
 public class UserController {
 
 	private UserService userService;
@@ -40,8 +34,7 @@ public class UserController {
 				+ env.getProperty("token.secret");
 	}
 
-	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, produces = {
-			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@PostMapping("/users")
 	public ResponseEntity<CreateUserResponse> createUser(@Valid @RequestBody CreateUserRequest request) {
 
 		UserDto userDto = modelMapper.map(request, UserDto.class);
